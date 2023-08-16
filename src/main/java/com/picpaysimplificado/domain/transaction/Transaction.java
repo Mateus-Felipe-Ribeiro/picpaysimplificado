333c9859b55e90a07df2a71f4a925229a696f621
Mateus Ribeiro <mateus@prs.net.br>
2023-08-16 09:41:59 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,27 @@
+package com.picpaysimplificado.domain.transaction;
+
+import jakarta.persistence.Entity;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
