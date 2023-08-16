c786124a201e9d93900a23152083d797d5a71984
Mateus Ribeiro <mateus@prs.net.br>
2023-08-16 09:42:15 -0300

    feat: create userRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optimal<User> findUserByDocument(String document);
+    Optimal<User> findUserById(Long id);
+}
