070e49e581be1f8266f382a301c8ae3c1e169459
Mateus Ribeiro <mateus@prs.net.br>
2023-08-16 09:44:44 -0300

    feat: create TransactionRepository

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package com.picpaysimplificado.repositories;
 
-import com.picpaysimplificado.domain.User;
+import com.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
